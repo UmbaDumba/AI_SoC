4e3ba79f075a2004714a44b9be1dff4201ca46ba
UmbaDumba <dkdi3104@gmail.com>
2025-05-21 16:53:00 +0900

    linux

--- cvtest/comp.py
+++ cvtest/comp.py
@@ -1,6 +1,23 @@
 import cv2
 
-s1 = cv2.imread('photo1.jpg')
-s2 = cv2.imread('photo2.jpg')
+s1 = cv2.imread('photo2.jpg')   #객체
+s2 = cv2.imread('photo1.jpg')   #배경
 
-cv2.absdiff()
+diff = cv2.absdiff(s1, s2)
+_, thre = cv2.threshold(diff, 120, 255, cv2.THRESH_BINARY)
+
+cann = cv2.Canny(diff, 100, 200)
+
+gray = cv2.cvtColor(thre, cv2.COLOR_BGR2GRAY)
+contours, r = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+contour_img = s2.copy()
+cv2.drawContours(contour_img, contours, -1, (0, 255, 0), 2)
+
+cv2.imshow("diff", diff)
+cv2.imshow("Threshold", thre)
+
+cv2.imshow("Contours", contour_img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
